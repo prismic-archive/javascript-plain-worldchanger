9a2439fbbe69d6222bbedba2a8f2e02d5fa16227
orlezio <brillard.hugo@gmail.com>
2014-04-23 15:16:14 +0200

    separation des fichiers js pour index.html

--- assets/javascripts/common.js
+++ assets/javascripts/common.js
@@ -1,9 +1,51 @@
-var urlAPI = 'https://worldchanger1.prismic.io/api';
-var master = 'Uxd1owEAAJ4oj-dR';
-var CTX = {};
+window.Common = {
+    getApi: function (callback) {
+        Prismic.Api('https://worldchanger1.prismic.io/api', function(err, api) {
+            callback(err, api);
+        });
+    },
 
+    getCtx: function(callback) {
+        this.getApi(function(err, api) {
+            var ctx = {
+                ref: api.data.master.ref,
+                api: api,
 
-$(function() {
+                linkResolver: function(ctx, documentLink) {
+                    if (documentLink.isBroken) return;
+
+                    if(documentLink.id == ctx.api.bookmarks['about']) {
+                      return '/about.html';
+                    }
+                    if(documentLink.id == ctx.api.bookmarks['faq']) {
+                      return '/faq.html';
+                    }
+                    if(documentLink.id == ctx.api.bookmarks['homepage']) {
+                      return '/homepage.html';
+                    }
+                    if(documentLink.id == ctx.api.bookmarks['pricing']) {
+                      return '/pricing.html';
+                    }
+                    if(documentLink.id == ctx.api.bookmarks['tour']) {
+                      return '/tour.html';
+                    }
+                }
+            };
+            callback(err, ctx);
+        });
+    },
+
+    getDocById: function(id, ctx, callback) {
+        ctx.api.form("everything")
+            .query('[[:d = at(document.id, "'+ ctx.api.bookmarks[id] +'")]]')
+            .ref(ctx.ref)
+            .submit(function (err, docs) {
+                callback(err, docs.results[0])
+            });
+    }
+};
+
+(function() {
 
     var cache = {};
     window.tmpl = function tmpl(str, data){
@@ -26,122 +68,7 @@ $(function() {
        
         return data ? fn( data ) : fn;
     };
-
-    
-
-
-
-    Prismic.Api(urlAPI, function(err, api){
-
-        // contexte
-
-        CTX = {
-            ref: master,
-            api: api,
-            maybeRef: (master && master != api.data.master.ref ? master : ''),
-            maybeRefParam: (master && master != api.data.master.ref ? '&ref=' + master : ''),
-        
-            linkResolver: function(ctx, documentLink) {
-                //return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
-                console.log('fthyfjy');
-                if (documentLink.isBroken) return;
-
-                /* Based on bookmark name of the document */
-                if(documentLink.id == ctx.api.bookmarks['about']) {
-                  return '/about.html' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.id == ctx.api.bookmarks['faq']) {
-                  return '/faq.html' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.id == ctx.api.bookmarks['homepage']) {
-                  return '/homepage.html' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.id == ctx.api.bookmarks['pricing']) {
-                  return '/pricing.html' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.id == ctx.api.bookmarks['tour']) {
-                  return '/tour.html' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-            }
-        }
-
-        // index.html
-
-        function getDocById(id, callback) {
-            api.form("everything")
-                .query('[[:d = at(document.id, "'+id+'")]]')
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results[0])
-                });
-        }
-    
-        function getFriendlyArgs(callback) {
-            api.form("arguments")
-                .query('[[:d = at(document.tags, ["userfriendly"])][:d = at(document.tags, ["featured"])]]')
-                .orderings("[my.argument.priority desc]")
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results)
-                });
-        }
-
-        function getDesignArgs(callback) {
-            api.form("arguments")
-                .query('[[:d = at(document.tags, ["design"])][:d = at(document.tags, ["featured"])]]')
-                .orderings("[my.argument.priority desc]")
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results)
-                });
-        }
-
-        function getQuestions(callback) {
-            api.form("questions")
-                .query('[[:d = at(document.tags, ["featured"])]]')
-                .orderings("[my.faq.priority desc]")
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results)
-                });
-        }
-
-        function getMinPrice(callback) {
-            api.form("plans")
-                .query('[[:d = at(document.type, "pricing")]]')
-                .orderings("[my.pricing.price]")
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results[0].getNumber('pricing.price'))
-                });
-        }
-
-        // tour
-
-        function getArgs(callback) {
-            api.form("arguments")
-                .orderings("[my.argument.priority desc]")
-                .ref(master)
-                .submit(function (err, docs) {
-                    callback(err, docs.results)
-                });
-        }
-
-    });
-});
-
-;(function($){
-
-    /* On the FAQ page, opens the right accordion item if the ID of the question is passed as the hash */
-    /* This can only be done through the front-end, as the hash is never passed to the server. */
-    $(document).ready(function(){
-        if ($('article#faq').length != 0) {
-            var hash = window.location.hash.substring(1);
-            $('#'+hash).addClass("in").css('height', 'auto');
-        }
-    });
-
-}( jQuery ));
+})();
 
 
 
--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -1,23 +1,64 @@
 $(function(){
+    
+    function getFriendlyArgs(ctx, callback) {
+        ctx.api.form("arguments")
+            .query('[[:d = at(document.tags, ["userfriendly"])][:d = at(document.tags, ["featured"])]]')
+            .orderings("[my.argument.priority desc]")
+            .ref(ctx.ref)
+            .submit(function (err, docs) {
+                callback(err, docs.results)
+            });
+    }
 
-    Prismic.Api(urlAPI, function(err, api){
+    function getDesignArgs(ctx, callback) {
+        ctx.api.form("arguments")
+            .query('[[:d = at(document.tags, ["design"])][:d = at(document.tags, ["featured"])]]')
+            .orderings("[my.argument.priority desc]")
+            .ref(ctx.ref)
+            .submit(function (err, docs) {
+                callback(err, docs.results)
+            });
+    }
 
-        getDocById(api.bookmarks['homepage'], function(err1, home) {
-            getFriendlyArgs(function(err2, friendlyArgs) {
-                getDesignArgs(function(err3, designArgs) {
-                    getQuestions(function(err4, questions) {
-                        getMinPrice(function(err5, minPrice) {
+    function getQuestions(ctx, callback) {
+        ctx.api.form("questions")
+            .query('[[:d = at(document.tags, ["featured"])]]')
+            .orderings("[my.faq.priority desc]")
+            .ref(ctx.ref)
+            .submit(function (err, docs) {
+                callback(err, docs.results)
+            });
+    }
+
+    function getMinPrice(ctx, callback) {
+        ctx.api.form("plans")
+            .query('[[:d = at(document.type, "pricing")]]')
+            .orderings("[my.pricing.price]")
+            .ref(ctx.ref)
+            .submit(function (err, docs) {
+                callback(err, docs.results[0].getNumber('pricing.price'))
+            });
+    }
+
+    Common.getCtx(function(err, ctx){
+
+        Common.getDocById('homepage', ctx, function(err1, home) {
+            getFriendlyArgs(ctx, function(err2, friendlyArgs) {
+                getDesignArgs(ctx, function(err3, designArgs) {
+                    getQuestions(ctx, function(err4, questions) {
+                        getMinPrice(ctx, function(err5, minPrice) {
                             var source = $('body').find('script[type="text/template"]').html();
                             var template = source ? tmpl(source) : undefined;
                             
-                            infos = {
+                            var infos = {
                                 friendlyArgs: friendlyArgs,
                                 designArgs: designArgs,
                                 questions: questions,
                                 home: home,
                                 minPrice: minPrice,
-                                ctx: CTX
+                                ctx: ctx
                             }
+
                             if(template) {
                                 $('#homepage').html(template(infos));
                             }
